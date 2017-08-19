e67d0db0e42f79077d8384c77132bda92ad7bdd6
Karina Katrich <katrich@dnt-lab.com>
2017-08-19 21:11:10 +0200

    Initial Commit

---
+++ MapboxTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapboxTest
+//
+//  Created by Karina on 8/19/17.
+//  Copyright © 2017 Karyna Katrich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

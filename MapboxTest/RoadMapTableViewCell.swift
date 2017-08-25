d36a0cea614e978891065db84920f9c0a29078e6
Karina Katrich <katrich@dnt-lab.com>
2017-08-26 00:47:35 +0200

    first commit

---
+++ MapboxTest/RoadMapTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RoadMapTableViewCell.swift
+//  MapboxTest
+//
+//  Created by Karina on 8/25/17.
+//  Copyright © 2017 Karyna Katrich. All rights reserved.
+//
+
+import UIKit
+
+class RoadMapTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
